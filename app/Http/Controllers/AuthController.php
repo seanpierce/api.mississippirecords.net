4e491af29abc900ed2cd99d4431f77058f15d64d
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-09 21:27:03 -0700

    Add b2b authentication method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -43,5 +43,33 @@ class AuthController extends Controller
 
         if (!$is_admin)
             abort(403, 'Unauthorized action.');
+	}
+	
+	public function allow_b2b(Request $request)
+    {
+        // get token from header
+        $token_header = $request->header('token');
+        
+        if (!$token_header)
+            abort(403, 'Unauthorized action.');
+        
+        // get token from db
+        $token = Token::where('token', $token_header)->first();
+
+        if (!$token)
+            abort(403, 'Unauthorized action.');
+
+        // get user from db
+        $user = User::where('id', $token->user_id)->first();
+
+        if (!$user)
+            abort(403, 'Unauthorized action.');
+
+        // is user class ADMIN or B2B?
+		$is_admin = $user->class == 'ADMIN';
+		$is_b2b = $user->class == 'B2B';
+
+        if (!$is_admin && !$is_b2b)
+            abort(403, 'Unauthorized action.');
     }
 }
