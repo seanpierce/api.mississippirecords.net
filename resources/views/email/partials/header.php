15c4719d800e45dc3da4668bbe76c6335cd7cbec
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-09 17:50:06 -0700

    Add email partials

---
+++ resources/views/email/partials/header.php
@@ -0,0 +1,6 @@
+<html>
+    <head>
+    </head>
+    <body style='font-size:16px;font-color:#333;font-family:Arial,Helvetica,sans-serif;'>
+    <div style='width:90%;max-width:500px;display:block;margin:auto;'>
+    <img src='https://staging-api.mississippirecords.store/uploads/mrecs-logo.png' style='width:200px;display:block;margin:auto'>
