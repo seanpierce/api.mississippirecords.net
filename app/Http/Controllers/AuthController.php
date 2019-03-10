d71904a1e7a61ada50a3f0f1920d4546a3257918
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-10 07:28:49 -0700

    Add token expiration logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,23 +20,7 @@ class AuthController extends Controller
 
     public function allow_admin(Request $request)
     {
-        // get token from header
-        $token_header = $request->header('token');
-        
-        if (!$token_header)
-            abort(403, 'Unauthorized action.');
-        
-        // get token from db
-        $token = Token::where('token', $token_header)->first();
-
-        if (!$token)
-            abort(403, 'Unauthorized action.');
-
-        // get user from db
-        $user = User::where('id', $token->user_id)->first();
-
-        if (!$user)
-            abort(403, 'Unauthorized action.');
+        $user = $this->get_user_from_valid_token($request);
 
         // is user class ADMIN?
         $is_admin = $user->class == 'ADMIN';
@@ -47,7 +31,19 @@ class AuthController extends Controller
 	
 	public function allow_b2b(Request $request)
     {
-        // get token from header
+        $user = $this->get_user_from_valid_token($request);
+
+        // is user class ADMIN or B2B?
+		$is_admin = $user->class == 'ADMIN';
+		$is_b2b = $user->class == 'B2B';
+
+        if (!$is_admin && !$is_b2b)
+            abort(403, 'Unauthorized action.');
+	}
+	
+	private function get_user_from_valid_token(Request $request)
+	{
+		// get token from header
         $token_header = $request->header('token');
         
         if (!$token_header)
@@ -57,19 +53,29 @@ class AuthController extends Controller
         $token = Token::where('token', $token_header)->first();
 
         if (!$token)
-            abort(403, 'Unauthorized action.');
+			abort(403, 'Unauthorized action.');
+			
+		// validate token expiration
+		$now = time();
+		$target = strtotime($token->updated_at);
+		$difference = $now - $target;
+
+		if ($difference > Expiration::one_hour)
+			abort(403, 'Token has expired.');
 
         // get user from db
         $user = User::where('id', $token->user_id)->first();
 
         if (!$user)
-            abort(403, 'Unauthorized action.');
+			abort(403, 'Unauthorized action.');
 
-        // is user class ADMIN or B2B?
-		$is_admin = $user->class == 'ADMIN';
-		$is_b2b = $user->class == 'B2B';
+		$token->save();	
+		return $user;
+	}
+}
 
-        if (!$is_admin && !$is_b2b)
-            abort(403, 'Unauthorized action.');
-    }
+abstract class Expiration
+{
+	const fifteen_minutes = 900;
+	const one_hour = 3600;
 }
