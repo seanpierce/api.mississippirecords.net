9394f5569f3cfa123c0042fb2bd210d84ec72086
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-09 21:32:11 -0700

    Add email templates

--- resources/views/email/send.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head></head>
-<body style="font-family: Arial, Helvetica, sans-serif;">
-<img src="https://api.mississippirecords.net/uploads/mississippi_fuck-your-progress.png">
-<h1><?php echo $title; ?></h1>
-<p><?php echo $content; ?></p>
-</body>
-</html>
