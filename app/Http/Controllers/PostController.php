3967376389b0670329845b960c2050165265da4c
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-11 19:38:50 -0700

    Im a dummy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post as Post;
 
-class ItemController extends Controller
+class PostController extends Controller
 {
     private $auth;
     /**
