e55c625f9b835e7e177b3818175040d0e92026e2
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-19 10:42:59 -0700

    Create featured item unit tests

---
+++ tests/FeaturedItemTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Models\Token as Token;
+use App\Models\Item as Item;
+
+class FeaturedItemTest extends TestCase
+{
+    /**
+     * /items [POST]
+     */
+    public function test_should_create_new_featured_item()
+    {
+        // create a user/ token combo
+        $token = factory('App\Models\Token')->create();
+
+        $item = Item::all()->last();
+
+        $parameters = [
+            'item_id' => $item->id,
+        ];
+
+        $response = $this->post("featured", $parameters, ['token' => $token->token])
+            ->response
+            ->getContent();
+
+        $content = filter_var($response, FILTER_VALIDATE_BOOLEAN);
+
+        $this->seeStatusCode(200);
+        $this->assertTrue($content);
+    }
+
+    /**
+     * /items [GET]
+     */
+    public function test_should_show_all_featured_items()
+    {
+        $this->get("featured");
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure([
+            '*' =>
+                [
+                    'id',
+                    'artist',
+                    'title',
+                    'created_at',
+                    'updated_at',
+                    'description',
+                    'basic_cost',
+                    'b2b_cost',
+                    'images',
+                    'audio',
+                    'quantity_available',
+                    'catalog',
+                    'category',
+                    'presale',
+                    'b2b_enabled',
+                    'direct_enabled',
+                ]
+            ]);
+    }
+}
