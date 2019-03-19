8bebbc33587309bd9071f7cff9d042750e65aa2d
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-19 10:24:43 -0700

    Add tests for update, delete and not found cases

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -94,4 +94,66 @@ class ItemTest extends TestCase
         $this->seeStatusCode(200);
         $this->assertTrue($content);
     }
+
+    /**
+     * /items [PUT]
+     */
+    public function test_should_update_existing_item()
+    {
+        // create a user/ token combo
+        $token = factory('App\Models\Token')->create();
+
+        $parameters = [
+            'id' => 1,
+            'artist' => 'Test Artist 2',
+            'title' => 'Test Title 2',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'basic_cost' => 2001,
+            'b2b_cost' => 1501,
+            'images' => ['my_image.jpg'],
+            'audio' => 'my_audio.mp3',
+            'quantity_available' => 100,
+            'catalog' => 'TEST-002',
+            'category' => 'mrecs',
+            'presale' => false,
+            'b2b_enabled' => false,
+            'direct_enabled' => true,
+        ];
+
+        $response = $this->put("items", $parameters, ['token' => $token->token])
+            ->response
+            ->getContent();
+
+        $content = filter_var($response, FILTER_VALIDATE_BOOLEAN);
+
+        $this->seeStatusCode(200);
+        $this->assertTrue($content);
+    }
+
+    /**
+     * /items [DELETE]
+     */
+    public function test_should_delete_existing_item()
+    {
+        // create a user/ token combo
+        $token = factory('App\Models\Token')->create();
+
+        $response = $this->delete("items/1", [], ['token' => $token->token])
+            ->response
+            ->getContent();
+
+        $content = filter_var($response, FILTER_VALIDATE_BOOLEAN);
+
+        $this->seeStatusCode(200);
+        $this->assertTrue($content);
+    }
+
+    /**
+     * /items/[id] [GET]
+     */
+    public function test_should_return_not_found_deleted_item()
+    {
+        $this->get("items/1");
+        $this->seeStatusCode(404);
+    }
 }
