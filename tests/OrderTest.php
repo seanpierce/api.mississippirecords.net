a781280052d08faedc00cb932936892bbcb35359
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-20 11:23:14 -0700

    create request international order endpoint test

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -2,21 +2,46 @@
 
 use App\Models\PasswordHash as PasswordHash;
 use App\Models\Item as Item;
+use App\Models\SubModels\OrderConfirmationItem as OrderConfirmationItem;
 
 class OrderTest extends TestCase
 {
+    private $faker;
+
+    public function __construct() {
+        parent::__construct();
+        $this->faker = Faker\Factory::create();
+    }
+
     /**
      * @group orders
      * /orders/international [POST]
      */
     public function test_should_request_an_international_order()
     {
-        $items = Item::paginate(3);
+        $items = [];
+
+        for ($i = 0; $i < 3; $i ++)
+        {
+            $item = new OrderConfirmationItem;
+            $item->id = 0;
+            $item->artist = $this->faker->firstNameFemale . ' ' . $this->faker->lastName;
+            $item->title = $this->faker->realText($maxNbChars = 20, $indexSize = 2);
+            $item->cost = $this->faker->numberBetween($min = 1000, $max = 3000);
+            $item->quantity_ordered = $this->faker->numberBetween($min = 10, $max = 30);
+            $item->quantity_available = 1000;
+            $item->available = true;
+            
+            $item->subtotal = $item->quantity_ordered * $item->cost;
+
+            array_push($items, $item);
+        }
+
 
         $parameters = [
             'name' => 'Test Customer',
             'email' => 'testemail@example.com',
-            'items' => $items
+            'items' => $items,
         ];
 
         $response = $this->post('orders/international', $parameters)
