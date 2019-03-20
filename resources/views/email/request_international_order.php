c78e8e1883aef394e09dfef484354bff811fa95a
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-20 11:22:56 -0700

    Create request international order email template

---
+++ resources/views/email/request_international_order.php
@@ -0,0 +1,47 @@
+<?php
+
+$table = "	
+<table style=width:100%;border-collapse:collapse;table-layout:fixed;>
+<thead style=border-bottom:1px;border-color:lightgrey;margin-bottom:12px;>
+	<tr>
+        <td style=padding-bottom:6px;border-bottom:solid;border-color:lightgray;text-align:left;>Item</td>
+        <td style=padding-bottom:6px;border-bottom:solid;border-color:lightgray;text-align:right;>Cost</td>
+        <td style=padding-bottom:6px;border-bottom:solid;border-color:lightgray;text-align:right;>QTY</td>
+        <td style=padding-bottom:6px;border-bottom:solid;border-color:lightgray;text-align:right;>Sub</td>
+	</tr>
+</thead>
+<tbody>
+";
+
+$row = 1;
+
+foreach ($items as $item) {
+    $even = $row % 2 == 0;
+
+    if ($even)
+        $table .= "<tr style='background:#ddd;'>";
+    else
+        $table .= "<tr>";
+
+	$table .= "
+        <td style=padding:4px;vertical-align:top;width:60%;>$item->artist - $item->title</td>
+        <td style=padding:4px;vertical-align:top;width:60%;text-align:right>\${$helpers->format_money($item->cost)}</td>
+        <td style=padding:4px;vertical-align:top;width:60%;text-align:right>$item->quantity_ordered</td>
+        <td style=padding:4px;vertical-align:top;width:60%;text-align:right>\${$helpers->format_money($item->subtotal)}</td>
+	</tr>
+    ";
+
+    $row += 1;
+}
+
+$table .= "
+    </tbody>
+    <p style='font-weight:bold;'>Order Total: \${$helpers->format_money($order_total)}</p>
+    ";
+
+?>
+
+<p>Hi <?php echo $name ?>,</p>
+<p>Thanks for requesting to place an international order. We'll contact you as soon as we can to discuss shipping options. We appreciate your support!</p>
+
+<?php echo $table; ?>
